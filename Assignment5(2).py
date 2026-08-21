469b130e6539a5b2e6f06f71055718f5fc8e2aa1
Pratikaware-rgb <rajawre3@gmail.com>
2026-08-21 09:50:38 +0530

    Create Assignment5(2).py

---
+++ Assignment5(2).py
@@ -0,0 +1,62 @@
+# Decorator
+def report_format(function):
+    def wrapper(self):
+        print("=" * 40)
+        print("REPORT GENERATOR")
+        print("=" * 40)
+
+        function(self)
+
+        print("=" * 40)
+        print("END OF REPORT")
+        print("=" * 40)
+    return wrapper
+
+
+# Report class
+class Report:
+
+    # Magic Method (Constructor)
+    def __init__(self, title, sections):
+        self.title = title
+        self.sections = sections
+
+    # Class Method
+    @classmethod
+    def sample_report(cls):
+        title = "Student Performance Report"
+
+        sections = [
+            "Student Name: Soham",
+            "Marks: 90",
+            "Grade: A"
+        ]
+
+        return cls(title, sections)
+
+    # Decorator
+    @report_format
+    def display(self):
+        print("Title:", self.title)
+        print()
+
+        for item in self.sections:
+            print(item)
+
+    # Magic Method
+    def __str__(self):
+        return f"Report Title: {self.title}"
+
+    # Magic Method
+    def __len__(self):
+        return len(self.sections)
+
+
+# Main Program
+report = Report.sample_report()
+
+print(report)
+print("Total sections:", len(report))
+print()
+
+report.display()
