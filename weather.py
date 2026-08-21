e84cba3d0cc25747ae5bde0d8f5d61231e776657
Pratikaware-rgb <rajawre3@gmail.com>
2026-08-21 09:57:10 +0530

    Create weather.py

---
+++ weather.py
@@ -0,0 +1,30 @@
+    def update(self, temperature):
+        print("Temperature =", temperature, "°C")
+
+
+# Subject Class
+class WeatherStation:
+
+    def __init__(self):
+        self.display = []
+
+    def register(self, obj):
+        self.display.append(obj)
+
+    def notify(self, temperature):
+        for d in self.display:
+            d.update(temperature)
+
+
+# Main Program
+
+d1 = Display()
+d2 = Display()
+
+station = WeatherStation()
+
+station.register(d1)
+station.register(d2)
+
+station.notify(28)
+station.notify(35)
